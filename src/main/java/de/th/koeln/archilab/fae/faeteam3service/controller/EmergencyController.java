a48771a7d564427c8486ec0309ddc9bbff97ad03
Sebastian <Strike.in@googlemail.com>
2019-11-11 19:31:37 +0100

    Fixed endpoints

--- src/main/java/de/th/koeln/archilab/fae/faeteam3service/controller/EmergencyController.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam3service/controller/EmergencyController.java
@@ -6,7 +6,6 @@ import de.th.koeln.archilab.fae.faeteam3service.repository.EmergencyRepository;
 import de.th.koeln.archilab.fae.faeteam3service.repository.MessageRepository;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -24,16 +23,16 @@ public class EmergencyController {
 
     @PostMapping("/emergency")
     public Emergency createEmergency(@Valid @RequestBody Emergency emergency) {
-        log.info("Trying to create emergency: " + emergency.toString());
 
-        //Not really nice. There should be a better solution
-        UUID newEmergency = emergencyRepository.save(emergency).getId();
-        return emergencyRepository.findById(newEmergency).map(emergency1 -> {
-            Message newMessage = new Message();
-            newMessage.setEmergency(emergency1);
-            messageRepository.save(newMessage);
-            return emergency1;
-        }).orElseThrow(() -> new ResourceNotFoundException("Something went wrong!"));
+        emergency = emergencyRepository.save(emergency);
+
+        Message msg = messageRepository.save(new Message());
+        emergency.addMessage(msg);
+
+        messageRepository.save(msg);
+
+        log.info("Created emergency: " + emergency.toString());
+        return emergency;
     }
 
     @GetMapping("/emergency")
@@ -43,7 +42,7 @@ public class EmergencyController {
     }
 
     @DeleteMapping("/emergency/{id}")
-    public void deleteEmergency(@RequestParam UUID id) {
+    public void deleteEmergency(@PathVariable UUID id) {
         log.info("Deleting Emergency with ID: " + id);
         emergencyRepository.deleteById(id);
     }
--- src/main/java/de/th/koeln/archilab/fae/faeteam3service/entity/answer/AnswerType.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam3service/entity/answer/AnswerType.java
@@ -4,11 +4,13 @@ package de.th.koeln.archilab.fae.faeteam3service.entity.answer;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Data
+@ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 class AnswerType {
     private String type;
