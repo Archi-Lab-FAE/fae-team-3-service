19b4aec9412d00fbada45292aea971722e182758
aletutto <t.alessandro@web.de>
2019-11-27 15:50:48 +0100

    POST-Methode für Nachrichten hinzugefügt
    Es ist nun möglich, einer Ausnahmesituation mehrere Nachrichten hinzuzufügen

--- src/main/java/de/th/koeln/archilab/fae/faeteam3service/controller/NachrichtController.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam3service/controller/NachrichtController.java
@@ -1,12 +1,15 @@
 package de.th.koeln.archilab.fae.faeteam3service.controller;
 
+import de.th.koeln.archilab.fae.faeteam3service.entity.ausnahmesituation.Ausnahmesituation;
 import de.th.koeln.archilab.fae.faeteam3service.entity.nachricht.Nachricht;
+import de.th.koeln.archilab.fae.faeteam3service.repository.AusnahmesituationRepository;
 import de.th.koeln.archilab.fae.faeteam3service.repository.NachrichtRepository;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @Log
 @RestController
@@ -15,14 +18,35 @@ public class NachrichtController {
     @Autowired
     private NachrichtRepository nachrichtRepository;
 
+    @Autowired
+    private AusnahmesituationRepository ausnahmesituationRepository;
+
+    // TODO: Nur für Testzwecke, löschen wenn nich mehr benötigt
+    @GetMapping("/level-2/ausnahmesituation/nachrichten")
+    public Iterable<Nachricht> getAllNachrichten() {
+        return nachrichtRepository.findAll();
+    }
+
     @GetMapping("/level-2/ausnahmesituation/{ausnahmesituationId}/nachrichten")
     public Iterable<Nachricht> getAllNachrichtenByAusnahmesituation(@PathVariable String ausnahmesituationId) {
         log.info("Suche Nachrichten mit der Ausnahmesituation ID: " + ausnahmesituationId);
         return nachrichtRepository.findByAusnahmesituation_aunamesituationId(ausnahmesituationId);
     }
 
-    @GetMapping("/level-2/ausnahmesituation/nachrichten")
-    public Iterable<Nachricht> getAllNachrichten() {
-        return nachrichtRepository.findAll();
+    @PostMapping("/level-2/ausnahmesituation/{ausnahmesituationId}/nachrichten")
+    public Nachricht addNachrichtToAusnahmesituation(@PathVariable String ausnahmesituationId, @Valid @RequestBody Nachricht nachricht) {
+        Optional<Ausnahmesituation> ausnahmesituation = ausnahmesituationRepository.findById(ausnahmesituationId);
+
+        if(ausnahmesituation.isPresent()) {
+            ausnahmesituation.get().addNachricht(nachricht);
+            nachricht.setAusnahmesituation(ausnahmesituation.get());
+
+            ausnahmesituationRepository.save(ausnahmesituation.get());
+            nachrichtRepository.save(nachricht);
+        }
+
+        log.info("Füge der Ausnahme mit der ID: " + ausnahmesituationId + " eine Nachricht hinzu.");
+
+        return nachricht;
     }
 }
