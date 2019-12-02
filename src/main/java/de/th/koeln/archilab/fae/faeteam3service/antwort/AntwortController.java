d8908099192a5c4cb370b262f0736663e2d19d5a
aletutto <t.alessandro@web.de>
2019-12-02 14:46:22 +0100

    Kleine Änderungen durch Review von Sebastian-Quast
    - Zusammenbauen der E-Mail Nachricht in der Klasse EmailServiceImpl verbessert
    - Automatisch erstellte Wildcard Imports zu Single Imports im AntwortController geändert

--- src/main/java/de/th/koeln/archilab/fae/faeteam3service/antwort/AntwortController.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam3service/antwort/AntwortController.java
@@ -5,7 +5,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Log
 @Tag(name = "Antwort", description = "Antwort API")
