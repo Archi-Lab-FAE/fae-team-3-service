e6a60d4292505615d7388bb1ac8551913daa15d7
Sebastian <Strike.in@googlemail.com>
2020-01-06 16:52:34 +0100

    Euraka aktiviert

--- src/main/java/de/th/koeln/archilab/fae/faeteam3service/FaeTeam3ServiceApplication.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam3service/FaeTeam3ServiceApplication.java
@@ -2,8 +2,10 @@ package de.th.koeln.archilab.fae.faeteam3service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableJpaAuditing
 public class FaeTeam3ServiceApplication {
