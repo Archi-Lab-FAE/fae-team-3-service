a2712f4b8c8b6bda727ffec9a545470083b70f60
Sebastian Quast <sebastian.quast@open-xchange.com>
2020-01-10 13:13:13 +0100

    Added server URL for openApi

--- src/main/java/de/th/koeln/archilab/fae/faeteam3service/configuration/OpenApiConfig.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam3service/configuration/OpenApiConfig.java
@@ -3,6 +3,9 @@ package de.th.koeln.archilab.fae.faeteam3service.configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,13 +16,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
+    private List<Server> configServer() {
+        Server server = new Server();
+        server.setUrl("https://api.fae.archi-lab.io/");
+        server.description("Generated server url");
+        return Arrays.asList(server);
+    }
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
             .components(new Components())
             .info(new Info().title("Nachrichtensystem API").description(
                 "OpenAPI 3 Dokumentation des Nachrichtensystems "
-            ).version("1"));
+            ).version("1"))
+            .servers(configServer());
     }
 
 }
