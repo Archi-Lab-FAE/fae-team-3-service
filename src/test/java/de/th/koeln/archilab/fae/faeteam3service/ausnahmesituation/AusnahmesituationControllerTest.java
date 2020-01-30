13caee6682efd2e74edc9210f205e17f68619935
Sebastian <Strike.in@googlemail.com>
2020-01-30 14:14:20 +0100

    @EmbeddedKafka zu Controller test hinzugefügt.

--- src/test/java/de/th/koeln/archilab/fae/faeteam3service/ausnahmesituation/AusnahmesituationControllerTest.java
+++ src/test/java/de/th/koeln/archilab/fae/faeteam3service/ausnahmesituation/AusnahmesituationControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -37,8 +38,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@EmbeddedKafka
 @Log
-public class WebMockTest {
+public class AusnahmesituationControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
