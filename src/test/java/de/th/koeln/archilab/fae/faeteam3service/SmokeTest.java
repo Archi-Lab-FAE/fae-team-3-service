ad28865275579d754087c73d916fcaadb68a3612
Sebastian <Strike.in@googlemail.com>
2020-01-30 12:36:06 +0100

    Smoke test dependencies angepasst

--- src/test/java/de/th/koeln/archilab/fae/faeteam3service/SmokeTest.java
+++ src/test/java/de/th/koeln/archilab/fae/faeteam3service/SmokeTest.java
@@ -4,15 +4,12 @@ import de.th.koeln.archilab.fae.faeteam3service.antwort.AntwortController;
 import de.th.koeln.archilab.fae.faeteam3service.ausnahmesituation.AusnahmesituationController;
 import de.th.koeln.archilab.fae.faeteam3service.nachricht.NachrichtController;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SmokeTest {
 
