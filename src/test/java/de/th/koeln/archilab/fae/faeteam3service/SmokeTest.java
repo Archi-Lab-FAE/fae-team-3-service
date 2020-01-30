3b372da6ede805c0cdc5746fdfed1fe4fc0747a0
Sebastian <Strike.in@googlemail.com>
2020-01-30 12:44:52 +0100

    Jupiter hinzugefügt

--- src/test/java/de/th/koeln/archilab/fae/faeteam3service/SmokeTest.java
+++ src/test/java/de/th/koeln/archilab/fae/faeteam3service/SmokeTest.java
@@ -3,7 +3,7 @@ package de.th.koeln.archilab.fae.faeteam3service;
 import de.th.koeln.archilab.fae.faeteam3service.antwort.AntwortController;
 import de.th.koeln.archilab.fae.faeteam3service.ausnahmesituation.AusnahmesituationController;
 import de.th.koeln.archilab.fae.faeteam3service.nachricht.NachrichtController;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -23,7 +23,7 @@ public class SmokeTest {
     private NachrichtController nachrichtController;
 
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws Exception {
         assertThat(ausnahmesituationController).isNotNull();
         assertThat(antwortController).isNotNull();
         assertThat(nachrichtController).isNotNull();
