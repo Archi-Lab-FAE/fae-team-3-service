a41e07bb2daf2d53702b5a33d8ea73cf48ba0161
Sebastian <Strike.in@googlemail.com>
2020-01-30 17:54:49 +0100

    Lombok Annotation hinzugefügt.

--- src/main/java/de/th/koeln/archilab/fae/faeteam3service/eventing/intern/DomainEvent.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam3service/eventing/intern/DomainEvent.java
@@ -1,9 +1,13 @@
 package de.th.koeln.archilab.fae.faeteam3service.eventing.intern;
 
 import de.th.koeln.archilab.fae.faeteam3service.core.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.ZonedDateTime;
 
+@Getter
+@Setter
 public class DomainEvent {
   private String id;
   private String key;
@@ -11,52 +15,4 @@ public class DomainEvent {
   private Long version;
   private String type;
   private AbstractEntity payload;
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getKey() {
-    return key;
-  }
-
-  public void setKey(String key) {
-    this.key = key;
-  }
-
-  public ZonedDateTime getTimestamp() {
-    return timestamp;
-  }
-
-  public void setTimestamp(ZonedDateTime timestamp) {
-    this.timestamp = timestamp;
-  }
-
-  public Long getVersion() {
-    return version;
-  }
-
-  public void setVersion(final Long version) {
-    this.version = version;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public void setType(String type) {
-    this.type = type;
-  }
-
-  public AbstractEntity getPayload() {
-    return payload;
-  }
-
-  public void setPayload(AbstractEntity payload) {
-    this.payload = payload;
-  }
 }
