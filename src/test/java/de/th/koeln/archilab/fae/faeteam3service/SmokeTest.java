a61fdc8b1eabfa6eaa01db5a1e9dd1773af8e635
Barto <@>
2020-01-30 13:20:14 +0100

    - removed NachrichtenController

--- src/test/java/de/th/koeln/archilab/fae/faeteam3service/SmokeTest.java
+++ src/test/java/de/th/koeln/archilab/fae/faeteam3service/SmokeTest.java
@@ -2,7 +2,6 @@ package de.th.koeln.archilab.fae.faeteam3service;
 
 import de.th.koeln.archilab.fae.faeteam3service.antwort.AntwortController;
 import de.th.koeln.archilab.fae.faeteam3service.ausnahmesituation.AusnahmesituationController;
-import de.th.koeln.archilab.fae.faeteam3service.nachricht.NachrichtController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,14 +18,10 @@ public class SmokeTest {
     @Autowired
     private AntwortController antwortController;
 
-    @Autowired
-    private NachrichtController nachrichtController;
-
     @Test
     public void contextLoads() throws Exception {
         assertThat(ausnahmesituationController).isNotNull();
         assertThat(antwortController).isNotNull();
-        assertThat(nachrichtController).isNotNull();
     }
 
 }
