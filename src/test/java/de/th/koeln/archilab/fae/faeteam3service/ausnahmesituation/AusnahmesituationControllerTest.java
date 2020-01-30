37fab1c7add4c2701eef87e565b52c662024548c
Barto <@>
2020-01-30 17:41:02 +0100

    okay jetzt aber wirklich alles gefixt

--- src/test/java/de/th/koeln/archilab/fae/faeteam3service/ausnahmesituation/AusnahmesituationControllerTest.java
+++ src/test/java/de/th/koeln/archilab/fae/faeteam3service/ausnahmesituation/AusnahmesituationControllerTest.java
@@ -27,9 +27,9 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Collections;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
